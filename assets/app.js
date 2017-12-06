7bba4891e899712236bdc640a29d6efaf034f68d
Florian Lenz <flenz81@googlemail.com>
2017-12-07 00:59:16 +0100

    [ui] swaped materialize with bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import Home from './components/Home.vue';
 
+Vue.use(Vuetify);
 Vue.component('home', Home);
 
 new Vue({
--- assets/components/Home.vue
+++ assets/components/Home.vue
@@ -1,5 +1,7 @@
 <template>
-    <h1>Hi</h1>
+    <div>
+
+    </div>
 </template>
 
 <script>
